8642784547568bd55c09ef70aa48d30f9264a0b1
Matthew Teolis <matthewteolis@gmail.com>
2016-01-24 17:40:01 -0500

    Initial commit

---
+++ database/migrations/2016_01_24_220041_courses_table_create.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CoursesTableCreate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+	    $table->string('title');
+	    $table->text('description');
+	    $table->integer('faculty_id')->unsigned()->foreign('faculty_id')->references('id')->on('faculties');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('courses');
+    }
+}
---
+++ resources/views/welcome.blade.php~
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Course Registration</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
