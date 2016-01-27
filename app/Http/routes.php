805fc75b3b2dbdb920e9dab547222b04202abb45
Matthew Teolis <matthewteolis@gmail.com>
2016-01-27 14:19:09 -0500

    Reverting to Fresh laravel install

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', 'CourseController@display');
-Route::get('/course_registration', 'CourseController@registration');
-Route::post('/course_register', ['as' => 'course_register', 'uses' => 'CourseController@register']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 /*
 |--------------------------------------------------------------------------
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,45 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Course Registration</title>
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Laravel</title>
 
-        <!--Import jQuery before materialize.js-->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <!--Import Google Icon Font-->
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Import materialize.css-->
-        <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
-        <!-- Compiled and minified JavaScript -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+        <style>
+            html, body {
+                height: 100%;
+            }
 
-      
-        
-        
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
 
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
     </head>
     <body>
-        {{-- @foreach($courses as $course)
-        <div>{{ $course->title }}</div>
-        @endforeach
-        --}}
-        @yield('content')
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
     </body>
 </html>
