9e8f6721c2857bd16caafceb1508a2a1113bed68
unknown <francis.bouchard91@gmail.com>
2016-02-08 17:55:12 -0500

    Angular Theme

--- resources/views/app.js
+++ resources/views/app.js
@@ -1,7 +1,15 @@
 /**
  * Created by fbouc_000 on 2/6/2016.
  */
-var app = angular.module('StarterApp', []);
+var app = angular
+    .module('StarterApp', ['ngMaterial'])
+
+    .config(function($mdThemingProvider, $mdIconProvider){
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('yellow');
+    });
 
 app.controller('AppCtrl', ['$scope',
     function ($scope) {
