128e98df04b0fd94535db714453df49d7151543e
Matthew Teolis <matthewteolis@gmail.com>
2016-02-09 15:26:18 -0500

    Added ngRoute to dashboard controller

--- resources/assets/angular/dashboard/dashboard.controller.js
+++ resources/assets/angular/dashboard/dashboard.controller.js
@@ -2,9 +2,9 @@
  * Created by matthew on 08/02/16.
  */
 
-var app = angular.module('StarterApp', ['ngMaterial']);
+var app = angular.module('StarterApp', ['ngMaterial', 'ngRoute']);
 
-app.controller('CourseController', ['$scope', '$http',
+app.controller('DashboardController', ['$scope', '$http',
 	function ($scope, $http) {
 		$scope.test = 'Francis forgot that semicolon though... ->';
 		$scope.true = true;
