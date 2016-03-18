69dfb9ced547d441c3206c2a52c92d8d5b8d317c
Francis Bouchard <francis.bouchard91@gmail.com>
2016-03-18 13:15:13 -0400

    Angular front end stuff

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html ng-app="StarterApp">
     <head>
-<!--        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>-->
+
         <title>Apollo</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400" rel="stylesheet" type="text/css">
 
         <!-- Angular Material style sheet -->
         <link rel="stylesheet" href="..\bower_components\angular-material\angular-material.css">
@@ -24,6 +24,7 @@
         <script src="..\resources\views\app.js"></script>
         <script src="..\resources\assets\angular\course\course.config.js"></script>
 
+        <link rel="stylesheet" type="text/css" href="..\public\main.css">
 
 
     </head>
@@ -40,14 +41,37 @@
     <md-content layout="row" flex>
     <md-content layout="row" flex>
 
-        <md-sidenav class="md-sidenav-left md-whiteframe-z2" md-component-id="left" md-is-open="open" flex>
-            <md-list ng-cloak>
-                <md-list-item class="secondary-button-padding" ng-href="/public/#/courses">
-                    <h3>See course list</h3>
+        <md-sidenav class="md-sidenav-left md-whiteframe-z2" layout="column" md-component-id="left" md-is-open="open" flex>
+            <div flex="5"></div>
+            <div flex="30">
+                <div layout="column" layout-align="center center">
+                        <div>
+                            <user-avatar></user-avatar>
+                        </div>
+                        <div>Firstname Lastname</div>
+                        <div>email@domainname.com</div>
+                </div>
+            </div>
+            <md-divider></md-divider>
+            <md-list class="md-dense" ng-cloak>
+                <md-list-item ng-href="/public/#/courses">
+                    <h5>View Courses</h5>
+                </md-list-item>
+                <md-list-item ng-href="/public/#/scheduler">
+                    <h5>Scheduler</h5>
+                </md-list-item>
+                <md-list-item ng-href="/public/#/academics">
+                    <h5>View My Academics</h5>
+                </md-list-item>
+                <md-list-item class="secondary-button-padding" ng-href="/public/#/curentschedule">
+                    <h5>View My Current Schedule</h5>
+                </md-list-item>
+                <md-list-item class="secondary-button-padding" ng-href="/public/#/sequence">
+                    <h5>View Sequences</h5>
                 </md-list-item>
             </md-list>
         </md-sidenav>
-        <div flex="25"></div>
+        <div flex="20"></div>
         <div class="content" ui-view flex>
             <!-- CONTENT GOES HERE -->
         </div>
